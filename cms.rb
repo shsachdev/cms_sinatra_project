7d7bb80d9317f58a23d8de1354aa175a9fbd6c40
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-27 20:52:29 -0800

    writing helper method to deal with erroneous paths

--- cms.rb
+++ cms.rb
@@ -6,6 +6,10 @@ root = File.expand_path("..", __FILE__)
 
 # set :public_folder, 'data' # for some reason this doesn't render format correctly
 
+def verify_path(file_path)
+  
+end
+
 get '/' do
   @files = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
@@ -17,8 +21,6 @@ get "/:filename" do
   file_path = root + "/data/" + params[:filename]
 
   headers["Content-Type"] = "text/plain"
+  verify_path(file_path)
   File.read(file_path)
-end
-
-def error_for_file_name
 end
