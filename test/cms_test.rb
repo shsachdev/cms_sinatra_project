835312fe86b21f3eb65fadc07bd1e24f7c00f6b0
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-27 20:17:53 -0800

    duhhh

---
+++ test/cms_test.rb
@@ -0,0 +1,19 @@
+ENV["RACK_ENV"] = "test"
+
+require "minitest/autorun"
+require "rack/test"
+
+require_relative "../cms"
+
+class CMSTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get "/"
+    assert_equal 200, last_response.status
+  end
+end
