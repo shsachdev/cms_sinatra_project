8a070e4d9fb46de487e3610326ce0b225cbe5ec3
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-27 21:01:43 -0800

    let's use pry yay

--- cms.rb
+++ cms.rb
@@ -1,14 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
 require "erubis"
+require "pry"
 
 root = File.expand_path("..", __FILE__)
 
 # set :public_folder, 'data' # for some reason this doesn't render format correctly
 
-def verify_path(file_path)
-  
-end
+
 
 get '/' do
   @files = Dir.glob(root + "/data/*").map do |path|
@@ -21,6 +20,5 @@ get "/:filename" do
   file_path = root + "/data/" + params[:filename]
 
   headers["Content-Type"] = "text/plain"
-  verify_path(file_path)
   File.read(file_path)
 end
