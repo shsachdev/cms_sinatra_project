5bb6ae946c91d91dc8aa095fcd11a3117539e7e7
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-27 19:56:57 -0800

    updated gem file

--- cms.rb
+++ cms.rb
@@ -12,3 +12,10 @@ get '/' do
   end
   erb :index
 end
+
+get "/:filename" do
+  file_path = root + "/data/" + params[:filename]
+
+  headers["Content-Type"] = "text/plain"
+  File.read(file_path)
+end
