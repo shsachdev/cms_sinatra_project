3d8f0cb52e444f0d19e617573ac3f61e6e074921
Shikhar Sachdev <shsachdev@gmail.com>
2019-12-04 20:51:30 -0800

    everything functioning as expected

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -23,17 +23,27 @@ class CMSTest < Minitest::Test
     FileUtils.rm_rf(data_path)
   end
 
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
   def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+
     get "/"
 
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
-    assert_includes last_response.body, "history.txt"
   end
 
   def test_viewing_text_document
+    create_document "history.txt", "Ruby 0.95 released"
+
     get "/history.txt"
 
     assert_equal 200, last_response.status
@@ -42,28 +52,29 @@ class CMSTest < Minitest::Test
   end
 
   def test_document_not_found
-    get "/notafile.ext" # Attempt to access a nonexistent file
+    get "/notafile.ext"
 
-    assert_equal 302, last_response.status # Assert that the user was redirected
+    assert_equal 302, last_response.status
 
-    get last_response["Location"] # Request the page that the user was redirected to
+    get last_response["Location"]
 
     assert_equal 200, last_response.status
     assert_includes last_response.body, "notafile.ext does not exist"
-
-    get "/" # Reload the page
-    refute_includes last_response.body, "notafile.ext does not exist" # Assert that our message has been removed.
   end
 
   def test_viewing_markdown_document
-    get "/gonzalez.md"
+    create_document "about.md", "# Ruby is..."
+
+    get "/about.md"
 
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "<p>&quot;This is <em>bongos</em>, indeed.&quot;</p>\n"
+    assert_includes last_response.body, "<h1>Ruby is...</h1>"
   end
 
   def test_editing_document
+    create_document "changes.txt"
+
     get "/changes.txt/edit"
 
     assert_equal 200, last_response.status
