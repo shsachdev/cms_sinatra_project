c757d041a4883b56cf48fe7a64c5d1736b8d8e7b
Shikhar Sachdev <shsachdev@gmail.com>
2019-12-04 20:40:17 -0800

    tests to now run in isolated environment

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -3,6 +3,7 @@ ENV["RACK_ENV"] = "test"
 require "minitest/autorun"
 require "rack/test"
 require 'minitest/reporters'
+require "fileutils"
 Minitest::Reporters.use!
 
 require_relative "../cms"
@@ -12,6 +13,14 @@ class CMSTest < Minitest::Test
 
   def app
     Sinatra::Application
+  end
+
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
   end
 
   def test_index
